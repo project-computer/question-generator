090c83a0cc7e7fe45ee7d3c5a84137e2a50fb0d1
chanakorn-aramsak <chanakorn.aramsak@gmail.com>
2024-08-16 22:49:49 +0700

    Add daisyUI lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 module.exports = {
   content: [
     './index.html',
@@ -8,5 +9,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui,],
 };
