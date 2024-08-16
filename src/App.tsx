ac656e852cb3146dd505ba294a4b4129d61164d5
chanakorn-aramsak <chanakorn.aramsak@gmail.com>
2024-08-16 22:51:45 +0700

    Format into rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { invoke } from '@tauri-apps/api/tauri'
+import { createSignal } from 'solid-js'
+import logo from './assets/logo.svg'
+import './App.css'
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [greetMsg, setGreetMsg] = createSignal('')
+  const [name, setName] = createSignal('')
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke('greet', { name: name() }))
   }
 
   return (
@@ -32,14 +32,14 @@ function App() {
 
       <form
         class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+        onSubmit={e => {
+          e.preventDefault()
+          greet()
         }}
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={e => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
         <button type="submit">Greet</button>
@@ -47,7 +47,7 @@ function App() {
 
       <p>{greetMsg()}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 /** @type {import('vite').UserConfig} */
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [solidPlugin()],
   css: {
     postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
+      plugins: [tailwindcss, autoprefixer]
     }
   }
 })
