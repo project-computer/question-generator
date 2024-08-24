01622c8776d73f9db1b931b53dc5858092656f87
chanakorn <chanakorn.aramsak@gmail.com>
2024-08-24 21:07:58 +0700

    Add some mock function to test the usage of invoke rust command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,14 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+#[tauri::command]
+fn greet() {
+  println!("I was invoked from JS!");
+}
 
 fn main() {
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .invoke_handler(tauri::generate_handler![greet])
+         .run(tauri::generate_context!())
+         .expect("failed to run app");
 }
