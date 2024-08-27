ebdac4b6abd542f09efb97684e09e935679c8d6c
chanakorn <chanakorn.aramsak@gmail.com>
2024-08-27 21:01:53 +0700

    Add mocking pdfs for testing and link invoke with button

--- src/components/AddQuestionButton.tsx
+++ src/components/AddQuestionButton.tsx
@@ -1,6 +1,26 @@
+import { invoke } from '@tauri-apps/api/tauri'
+
 const AddQuestionButton = () => {
+  const handleClick = async () => {
+    try {
+      const response = await invoke('merge_documents')
+      if (response) {
+        console.log('Merged documents successfully')
+      }
+      return true
+    } catch (error) {
+      console.error('Error:', error)
+      return false
+    }
+  }
+  const handleClickWrapper = () => {
+    void handleClick()
+  }
   return (
-    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+    <button
+      class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+      onClick={handleClickWrapper}
+    >
       Button
     </button>
   )
